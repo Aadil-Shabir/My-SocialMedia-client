b9f37a5b54454edeafc311e0543b852a29a86a4a
unknown <aadil.shabir13@gmail.com>
2021-12-07 19:29:40 +0500

    edit proxy and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -9,7 +9,7 @@ import {
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000/",
+  uri: "https://morning-sands-88107.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
